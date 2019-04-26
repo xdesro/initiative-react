b1e4d756e6e82bece204c86a6f0f1ba0f21b9508
brynipoo <bryn.newell@gmail.com>
2019-04-26 14:24:43 -0600

    skeleton for creating a new character

--- src/Components/Dice.js
+++ src/Components/Dice.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Dice.scss';
 
