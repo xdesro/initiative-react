3d854c7fedf0372d5f042069f09390f493abd4b0
Henry Desroches <henry.desroches@promontech.com>
2019-05-23 15:28:01 -0600

    🔥  - Beginnings of character list consumption using firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBxcCoOLDwqSPKj1_JwskbzTy85HKxEjIE',
+  authDomain: 'crit-fail.firebaseapp.com',
+  databaseURL: 'https://crit-fail.firebaseio.com',
+  projectId: 'crit-fail',
+  storageBucket: 'crit-fail.appspot.com',
+  messagingSenderId: '84514988076',
+  appId: '1:84514988076:web:d00dd877af6e02f8'
+};
+export default firebaseConfig;
